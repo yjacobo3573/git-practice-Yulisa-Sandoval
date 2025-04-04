9487b435a6cc835a239f3a7943169973556e501f
yjacobo3573 <90855493+yjacobo3573@users.noreply.github.com>
2025-04-04 09:34:21 -0500

    used pylint to detect some static errors.Added a newline after end of code.etc..

--- Static-Dynamic-Analysis-Exercise/main.py
+++ Static-Dynamic-Analysis-Exercise/main.py
@@ -1,8 +1,19 @@
+"""This is an example of a program with many costly defects"""
+
 import math
 import random
 
 
 def expensive_op(n):
+    """
+        Performs a computationally expensive operation by summing i * n for i in range(1000).
+
+        Args:
+            n (int): A multiplier applied to each value from 0 to 999.
+
+        Returns:
+            int: The total sum of i * n for i in range(1000).
+        """
     total = 0
     for i in range(1000):
         total += i * n
@@ -10,6 +21,15 @@ def expensive_op(n):
 
 
 def slow_func(lst):
+    """
+       Runs expensive_op on each index of the input list.
+
+       Args:
+           lst (list): Input list (only length is used).
+
+       Returns:
+           list: Results from expensive_op(i) for each index i.
+       """
     result = []
     for i in range(len(lst)):
         result.append(expensive_op(i))
@@ -17,6 +37,12 @@ def slow_func(lst):
 
 
 def unused_function():
+    """
+        Adds two numbers and returns the result.
+
+        Returns:
+            int: The sum of 10 and 20.
+        """
     x = 10
     y = 20
     z = x + y
@@ -24,8 +50,9 @@ def unused_function():
 
 
 def main():
-    numbers = list(range(1000))
-    output = slow_func(numbers)
+    """
+       Executes the main routine by running slow_func on a list of 1000 numbers.
+       """
     print("Done")
 
 
